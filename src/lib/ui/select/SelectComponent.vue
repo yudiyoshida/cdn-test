f0b3518baee6124c609da8a9e777b5325b7b505b
Yudi Matsuguma <yudimatsuguma@gmail.com>
2025-07-25 15:59:30 -0300

    feat: add Select component and its subcomponents for enhanced selection functionality

---
+++ src/lib/ui/select/SelectComponent.vue
@@ -0,0 +1,42 @@
+<!-- eslint-disable @typescript-eslint/no-explicit-any -->
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../select';
+
+defineProps<{
+  items: any[];
+  itemKey: string;
+  itemLabel: string;
+}>();
+
+const emit = defineEmits<{
+  (e: 'selected', value: string): void;
+}>();
+
+function handleSelect(value: string) {
+  emit('selected', value);
+}
+</script>
+
+<template>
+  <Select>
+    <SelectTrigger class="w-full mb-4">
+      <SelectValue placeholder="Filtrar por categoria" />
+    </SelectTrigger>
+    <SelectContent class="bg-white">
+      <SelectItem
+        v-for="item in items"
+        :key="item[itemKey]"
+        :value="item[itemKey]"
+        @select="handleSelect(item[itemKey])"
+      >
+        {{ item[itemLabel] }}
+      </SelectItem>
+    </SelectContent>
+  </Select>
+</template>
---
+++ src/lib/ui/select/index.ts
@@ -0,0 +1,12 @@
+export { default as SelectContent } from './components/SelectContent.vue';
+export { default as SelectGroup } from './components/SelectGroup.vue';
+export { default as SelectItem } from './components/SelectItem.vue';
+export { default as SelectItemText } from './components/SelectItemText.vue';
+export { default as SelectLabel } from './components/SelectLabel.vue';
+export { default as Select } from './components/SelectRoot.vue';
+export { default as SelectScrollDownButton } from './components/SelectScrollDownButton.vue';
+export { default as SelectScrollUpButton } from './components/SelectScrollUpButton.vue';
+export { default as SelectSeparator } from './components/SelectSeparator.vue';
+export { default as SelectTrigger } from './components/SelectTrigger.vue';
+export { default as SelectValue } from './components/SelectValue.vue';
+
